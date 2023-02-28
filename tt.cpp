330549587d2cfb6765158f26256f42d7d11f2c95
Martin Hrubý <hrubym@fit.vutbr.cz>
2023-02-28 14:19:30 +0100

    bude cistka

---
+++ tt.cpp
@@ -0,0 +1,34 @@
+#include "mawsgocpp.hpp"
+
+// -------------------------------------------------------------------------------
+//
+using namespace mawscpp;
+
+mawscpp::Globals mawscpp::_G;
+
+//
+int main() {
+    //
+    EManCFG cfg;
+
+    cfg.defaultS3Bucket = "mawsgo";
+    cfg.projectID = "cpp-testing";
+
+    //
+    EMan eman(cfg);
+
+    //
+    eman().experimentID = "uzfunguje-2";
+
+    //
+    eman.addFile(EManFile("pes.csv", "tralaala,1,koza"));
+
+    //
+    if (eman.post() == false) {
+        //
+        printf("Nejaky errror\n");
+    }
+
+    //
+    return 0;
+}
